e042ca8fff5a0b9ac6516dc95976852b60219356
Sohail Abdul Sattar <40142350+SohailSattar@users.noreply.github.com>
2020-07-09 19:02:15 +0400

    Add files via upload
    Added Program.cs and Startup.cs

---
+++ BookStoreAPI/BookStoreAPI/Startup.cs
@@ -0,0 +1,123 @@
+using AutoMapper;
+using BookStoreAPI.Logic;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using Swashbuckle.AspNetCore.Filters;
+using System;
+using System.IO;
+
+namespace BookStoreAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddFluentValidation(fv =>
+            {
+                fv.RegisterValidatorsFromAssemblyContaining<AuthorValidator>();
+                fv.RegisterValidatorsFromAssemblyContaining<BookValidator>();
+            });
+
+            #region Serilog
+
+            services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
+
+            #endregion
+
+            #region Swagger
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+                // [SwaggerRequestExample] & [SwaggerResponseExample]
+                // version < 3.0 like this: c.OperationFilter<ExamplesOperationFilter>(); 
+                // version 3.0 like this: c.AddSwaggerExamples(services.BuildServiceProvider());
+                // version > 4.0 like this:
+                c.ExampleFilters();
+
+                c.OperationFilter<AddHeaderOperationFilter>("correlationId", "Correlation Id for the request", false); // adds any string you like to the request headers - in this case, a correlation id
+                c.OperationFilter<AddResponseHeadersFilter>(); // [SwaggerResponseHeader]
+
+                var filePath = Path.Combine(AppContext.BaseDirectory, "WebApi.xml");
+                c.IncludeXmlComments(filePath); // standard Swashbuckle functionality, this needs to be before c.OperationFilter<AppendAuthorizeToSummaryOperationFilter>()
+
+                c.OperationFilter<AppendAuthorizeToSummaryOperationFilter>(); // Adds "(Auth)" to the summary so that you can see which endpoints have Authorization
+                                                                              // or use the generic method, e.g. c.OperationFilter<AppendAuthorizeToSummaryOperationFilter<MyCustomAttribute>>();
+
+                // add Security information to each operation for OAuth2
+                c.OperationFilter<SecurityRequirementsOperationFilter>();
+                // or use the generic method, e.g. c.OperationFilter<SecurityRequirementsOperationFilter<MyCustomAttribute>>();
+
+                // if you're using the SecurityRequirementsOperationFilter, you also need to tell Swashbuckle you're using OAuth2
+                c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Description = "Standard Authorization header using the Bearer scheme. Example: \"bearer {token}\"",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+            });
+
+            services.AddSwaggerExamplesFromAssemblyOf(typeof(Author));
+            services.AddSwaggerExamplesFromAssemblyOf(typeof(Book));
+            services.AddSwaggerExamplesFromAssemblyOf(typeof(BookSale));
+
+            #endregion
+
+            services.AddTransient<IValidator<Author>, AuthorValidator>();
+
+            services.AddAutoMapper(typeof(Startup));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerfactory)
+        {
+            #region Swagger UI
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = "swagger";
+                c.DocumentTitle = "My Swagger UI";
+                c.SwaggerEndpoint("v1/swagger.json", "v1");
+            });
+
+            #endregion
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            loggerfactory.AddSerilog();
+        }
+    }
+}
